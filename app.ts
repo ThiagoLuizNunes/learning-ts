113d68491df3cc7fe8170480d7bc5f0e0a7fb94c
thiagoluiznunes <thiagoluiz.dev@gmail.com>
2018-05-07 15:32:01 -0300

    add first example

---
+++ app.ts
@@ -0,0 +1,14 @@
+let message: string = 'Help me Obi-Wan Kenogi, you are my only hop!';
+
+console.log(message);
+
+let episode: number = 0;
+episode = episode + 1;
+
+
+let isEnoughToBeatMF = function (parsecs: number): boolean {
+  return parsecs < 12;
+}
+
+let distance = 14;
+console.log(`Is ${distance} parsecs enough to beat Millenium Falcon?`);
---
+++ dist/app.js
@@ -0,0 +1,9 @@
+var message = 'Help me Obi-Wan Kenogi, you are my only hop!';
+console.log(message);
+var episode = 0;
+episode = episode + 1;
+var isEnoughToBeatMF = function (parsecs) {
+    return parsecs < 12;
+};
+var distance = 14;
+console.log("Is " + distance + " parsecs enough to beat Millenium Falcon?");
